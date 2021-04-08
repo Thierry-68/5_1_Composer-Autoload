afc96b56394ee50042c57f9032ed4e1a1f99b3b0
Thierry-68 <thierry.sandre68@gmail.com>
2021-04-08 10:14:24 +0200

    Update 5.2 Composer 2 - Package Management

--- public/index.php
+++ public/index.php
@@ -3,11 +3,16 @@
 require_once '../vendor/autoload.php';
 
 use App\Wcs\Hello;
+use HelloWorld\SayHello;
 
 //require_once("../src/Hello.php");
 //require_once "/vendor/autoload.php";
 //Autoloader::register();
 
 $goodDay=new Hello();
-
+echo "<h1>My Hello word</h1>";
 echo $goodDay->talk();
+echo "<h1>Hello word by Sir ehime</h1>";
+echo SayHello::world();
+
+
